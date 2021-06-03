a38cd3fe522fdd6cab19c5259b42e75022eec2c6
Meagan Lang <langmm.astro@gmail.com>
2021-06-02 18:41:52 -0700

    Fixed temp cmake that was added Replaced image with the correct one in intro notebook Added slide on the model submission form Added section to misc notebook on make/cmake models Added section to misc notebook on performance overhead

---
+++ models/sine_model.f90
@@ -0,0 +1,5 @@
+function call_sine(x) result(y)
+  real, intent(in) :: x
+  real :: y
+  y = sin(x)
+end function call_sine
---
+++ models/sine_model.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def call_sine(x):
+    y = np.sin(x)
+    return y
